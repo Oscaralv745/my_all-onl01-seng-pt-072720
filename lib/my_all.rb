d95d4c1afeee740d040d034ae7cf34ae1a018223
Oscar Alvarez <gavinalv6@gmail.com>
2020-08-03 15:52:14 +0000

    Automatically backed up by Learn

--- lib/my_all.rb
+++ lib/my_all.rb
@@ -2,8 +2,14 @@ require 'pry'
 
 def my_all?(collection)
   i = 0 
+  block_return_value = []
   while i < collection.length 
   yield(collection[i])
   i = i + 1 
   end 
+  if block_return_value.include(false)
+    false
+  else
+    true 
+  end
 end
